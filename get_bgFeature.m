5dcbb563eb8701759b0557ffa9a36e524c5e6b11
Annzstbl <345155252@qq.com>
2018-10-05 00:01:47 +0800

    '2018-10-04'

--- get_bgFeature.m
+++ get_bgFeature.m
@@ -1,43 +1,18 @@
-function [ bgFeature , index, bgWindowPos ] = get_bgFeature( img,  box,  myParams)
+function [ bgImg ] = get_bgFeature( img,  box,  myParams)
     imgSz = size(img);
     objSz = [box(3)-box(1)+1, box(4)-box(2)+1];
     bgSz = objSz * myParams.padding;
-    bgArea = prod(objSz);
-    extendSz = round((bgSz - objSz)/2);
-    %bgBox objBox的padding倍
-    bgBox(1) = max(round(box(1) - extendSz(1) + 1), 1);
-    bgBox(3) = min(round(box(3) + extendSz(1) - 1), imgSz(2));
-    bgBox(2) = max(round(box(2) - extendSz(2) + 1), 1);
-    bgBox(4) = min(round(box(4) + extendSz(2) - 1), imgSz(1));
+    pos(1) = round(box(1) + objSz(1)/2);
+    pos(2) = round(box(2) + objSz(2)/2);
     
-    % X1：背景框的x坐标取值；X2：x坐标取值+objsize
-    X = bgBox(1):bgBox(3) - objSz(1) + 1;
-    Y = bgBox(2):bgBox(4) - objSz(2) + 1;
-    [X1, Y1]=meshgrid(X, Y);
-    X1 = reshape(X1, [numel(X1),1]);
-    Y1 = reshape(Y1, [numel(Y1),1]);
-    X2 = X1 + objSz(1)-1;
-    Y2 = Y1 + objSz(2)-1;
+    X = floor(pos(1)) + (1:bgSz(1)) - floor(bgSz(1)/2);
+    Y = floor(pos(2)) + (1:bgSz(2)) - floor(bgSz(2)/2);
     
-    bgWindowPos = [X1, Y1, X2, Y2];
-    featureSz = prod(floor(objSz/8)) * 32;
-    X1num = numel(X1);
-    index = zeros(numel(X1),1);
-    bgFeature = zeros(X1num, featureSz);
-    for i = 1:numel(X1)
-        bgPos = bgWindowPos(i,:);
-        x1 = max(bgPos(1), box(1));
-        y1 = max(bgPos(2), box(2));
-        x2 = min(bgPos(3), box(3));
-        y2 = min(bgPos(4), box(4));
-        overlapArea = (x2 - x1) * (y2 - y1);
-        overlapRate = overlapArea / bgArea;
-        if overlapRate > myParams.overlapRateLimit
-            index(i) = i;
-        end
-        bg = im2single(img(bgPos(2):bgPos(4), bgPos(1):bgPos(3), :));
-        bgFeature(i,:) = reshape(fhog(bg),[1,featureSz]);
-    end     
-    index(index==0)=[];
+    X(X < 1) = 1;
+    Y(Y < 1) = 1;
+    X(X > imgSz(1)) = imgSz(1);
+    Y(Y > imgSz(2)) = imgSz(2);
+    
+    bgImg = img(Y, X, :);
 end
 
--- kernelSolve.m
+++ kernelSolve.m
@@ -1,7 +1,18 @@
-function [ kernel, W ] = kernelSolve( obFeature, bgFeature, kernelParams )
-
-    [obNum, obDim] = size(obFeature);
-    [bgNum, bgDim] = size(bgFeature);
+function [ kernel, W ] = kernelSolve( xf, yf, kernelParams )
+    
+	N = size(xf,1) * size(xf,2);
+	xx = xf(:)' * xf(:) / N;  %squared norm of x
+	yy = yf(:)' * yf(:) / N;  %squared norm of y
+	
+	%cross-correlation term in Fourier domain
+	xyf = xf .* conj(yf);
+	xy = sum(real(ifft2(xyf)), 3);  %to spatial domain
+	
+	%calculate gaussian response for all positions, then go back to the
+	%Fourier domain
+    sigma = kernelParams.sigma;
+	kf = fft2(exp(-1 / sigma^2 * max(0, (xx + yy - 2 * xy) / numel(xf))));
+    
     Wo = ones(obNum) / obNum;
     Wb = ones(bgNum) / bgNum;
     W = mdiag(Wo, Wb);
